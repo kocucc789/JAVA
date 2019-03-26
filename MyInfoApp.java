3e478b211e7611cf59067aea9286953ca1b9c62e
kocucc789 <49758@naver.com>
2019-03-27 02:10:52 +0900

    자신의 이름,나이,학과 출력하기
    명품자바p.52-자신의 이름,나이,학과 출력하기

---
+++ MyInfoApp.java
@@ -0,0 +1,15 @@
+
+public class MyInfoApp {
+
+	public static void main(String[] args) {
+		String name="¹Ú¹Ì¸®";
+		int age=21;
+		String Department="IT";
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(Department);
+		
+	}
+
+}
